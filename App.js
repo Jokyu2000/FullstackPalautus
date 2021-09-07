54612741d496ebb004c17bec3d74f5d0e29e24da
Jokyu2000 <namikokonttinen@gmail.com>
2021-09-07 20:24:46 +0300

    1.1

---
+++ App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Header = (props) => {
+  return(
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+const Content = (props) => {
+  return(
+    <div>
+      <p>
+        {props.content}{props.exercise}
+      </p>
+    </div>
+  )
+}
+const Total = (props) => {
+  return(
+    <div>
+      <p>
+        Number of exercises{props.exercise1}+{props.exercise2}+{props.exercise3}
+      </p>
+    </div>
+  )
+}
+const App = () =>{
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+  return (
+  <div>
+    <Header course={course}/>
+    <Content content={part1} exercise={exercises1}/>
+    <Content content={part2} exercise={exercises2}/>
+    <Content content={part3} exercise={exercises3}/>
+    <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3}/>
+  </div>
+  )
+  }
+  export default App;
